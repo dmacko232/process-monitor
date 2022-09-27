2f4603b2864c53ef10147b32aae65ddd5140b448
Dominik Macko <xmacko1@fi.muni.cz>
2022-09-27 14:04:32 +0200

    Add data saving.

---
+++ process_monitor/io.py
@@ -0,0 +1,11 @@
+from typing import List, Tuple
+import json
+import os
+
+def save_json(data: List[Tuple[float, int, int]], out_path: str) -> None:
+
+    out_dirname = os.path.dirname(out_path)
+    if out_dirname: # dont create in case its empty string
+        os.makedirs(os.path.dirname(out_path), exist_ok=True) # create dir if needed
+    with open(out_path, "w") as out_handle:
+        json.dump(data, out_handle, indent=2)
